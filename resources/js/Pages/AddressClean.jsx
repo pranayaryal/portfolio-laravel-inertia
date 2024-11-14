3207b57d17afac75318255f28fd61d0d6cae556a
pranayaryal <drpranayaryal@gmail.com>
2024-11-14 08:25:28 -0500

    add validation code

--- resources/js/Pages/AddressClean.jsx
+++ resources/js/Pages/AddressClean.jsx
@@ -103,32 +103,47 @@ export default function AddressClean({ uspsKey, uspsSec }) {
         return true;
 
     }
+
+    const onAddressChangeHandler = (field, value) => {
+        let state = {
+            [field]: {
+                value,
+                error: '',
+            },
+        };
+
+        setAddress({ ...address, ...state });
+    };
     return (
         <MainLayout>
             <div className="min-h-screen">
                 <div className="mt-6 relative w-full max-w-2xl lg:max-w-7xl">
                     <img src='./usps.svg' className='w-28 h-28' />
-                    <h1 class='text-3xl font-semibold mt-8'>Using the USPS Address Cleaner</h1>
-                    <p class='mt-8 text-xs text-black/50'>Enter an address. You can copy from <a class="text-indigo-500" href="https://maps.google.com" target="__blank">Google Maps</a></p>
-                    <div class='flex flex-col md:flex-row md:justify-start items-start space-y-8 md:space-y-0 md:space-x-40'>
+                    <h1 className='text-3xl font-semibold mt-8'>Using the USPS Address Cleaner</h1>
+                    <p className='mt-8 text-xs text-black/50'>Enter an address. You can copy from <a className="text-indigo-500" href="https://maps.google.com" target="__blank">Google Maps</a></p>
+                    <div className='flex flex-col md:flex-row md:justify-start items-start space-y-8 md:space-y-0 md:space-x-40'>
                         <div className='flex flex-col mt-2 w-[90%] md:w-1/3'>
                             <InputLabel value='Address' className='text-xs text-black/45' />
-                            <TextInput className='outline-none' />
-                            <span
-                                id='line1SmallLabel'
-                                className='text-xs text-gray-500'>Street address, P.0 box or military address</span>
+                            <input
+                                value={address.street.value}
+                                onChange={(e) => onAddressChangeHandler("street", e.target.value)}
+                                className={`text-xs mt-1 px-4 py-2 border outline-none ${address.street.error ? 'border-red-300' : 'border-gray-200'}`}
+                            />
+                            {(!address.street.error) && <span className='text-xs text-gray-500'>Street address, P.0 box or military address</span>}
+                            {address.street.error && <span className='text-xs text-red-400'>{address.street.error}</span>}
                             <div className="flex space-x-4 mt-4 w-[90%]">
                                 <div className="w-1/2">
                                     <InputLabel value='Town/City' className='text-xs text-black/45' />
                                     <input name='city'
-                                        value=''
-                                        id='city'
-                                        className='text-xs mt-1 px-4 py-2 border border-gray-400 rounded outline-none' />
+                                        onChange={(e) => onAddressChangeHandler("city", e.target.value)}
+                                        value={address.city.value}
+                                        className={`text-xs mt-1 px-4 py-2 border outline-none ${address.city.error ? 'border-red-300' : 'border-gray-200'}`} />
                                 </div>
                                 <div className="w-1/2">
                                     <InputLabel value='Postal Code' className='text-xs text-black/45' />
-                                    <input id='zip' name='zip' value=''
-                                        className='text-xs mt-1 px-4 py-2 border outline-none border-gray-400' />
+                                    <input id='zip' name='zip' value={address.zip.value}
+                                        onChange={(e) => onAddressChangeHandler("zip", e.target.value)}
+                                        className={`text-xs mt-1 px-4 py-2 border outline-none ${address.zip.error ? 'border-red-300' : 'border-gray-200'}`} />
 
                                 </div>
                             </div>
@@ -137,14 +152,16 @@ export default function AddressClean({ uspsKey, uspsSec }) {
                                 <select
                                     name='state'
                                     id='state'
-                                    className='px-3 py-2 bg-white border appearance-none font-sans tracking-wide text-md border-gray-200'>
+                                    className={`px-3 py-2 bg-white border appearance-none font-sans tracking-wide text-md ${address.state.error ? 'border-red-300' : 'border-gray-200'}`}
+                                    onChange={(e) => onAddressChangeHandler("state", e.target.value)}>
                                     <option name="state"
-                                        value="" class='text-black'>
+                                        value={address.state.value} className='text-black'>
                                         Select state</option>
                                     {Object.keys(states).map(s => {
                                         return <option name="state"
+                                            key={s}
                                             value={s}
-                                            selected={s} class='text-black'>{states[s]}</option>
+                                            className='text-black'>{states[s]}</option>
 
                                     })}
                                 </select>
@@ -155,6 +172,7 @@ export default function AddressClean({ uspsKey, uspsSec }) {
 
                             </div>
                             <button
+                                onClick={callValidate}
                                 id='validate-button'
                                 className='bg-black text-white w-full md:w-[50%] text-sm py-3 px-3 ml-auto mr-auto mt-8 hover:bg-gray-800'>
                                 Validate
